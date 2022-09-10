e3a62362fcf82759cde87088b11878b77093b196
tochibedford <musicofbyte@gmail.com>
2022-09-10 09:23:53 +0100

    enumerations

---
+++ enumerations/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:#?}", four);
+}
+
+fn route(ip_kind: IpAddrKind) {
+    
+}
