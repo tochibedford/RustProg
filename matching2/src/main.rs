0548723c0211a7253fd56e87b20d16e4684b92ba
tochibedford <musicofbyte@gmail.com>
2022-09-10 14:49:28 +0100

    match control

---
+++ matching2/src/main.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    California,
+    NJ
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("This quarter is from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    dbg!(value_in_cents(Coin::Quarter(UsState::California)));
+}
